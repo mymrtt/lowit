75d8633c6cfb496e19c36a503387126bb4257aef
mymrtt <mymrtt@gmail.com>
2019-01-26 22:28:20 -0200

    1st commit

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return(
+      <header className="container_header">
+        <img src="../assets/lowlogo.png" className="logolowit" alt="Logo Low-It" />
+        <div className="container_header-itens">
+          <div>
+            {/* <img src="" className="" alt="configuração" /> */}
+            <p>configuração</p>
+          </div>
+          <div>
+            {/* <img src="" className="" alt="profile" /> */}
+            <p>profile</p>
+          </div>
+          <div>
+            {/* <img src="" className="" alt="logout" /> */}
+            <p>logout</p>
+          </div>
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header;
