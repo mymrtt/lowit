99568a075d6d1d10bd16c3c5904a33f38b01de1e
mymrtt <mymrtt@gmail.com>
2019-02-15 09:12:17 -0200

    update filter screen  dashboard

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -5,83 +5,93 @@ var treeData = "";
 
 class Filter extends Component {
 
-  constructor(props){
-    super(props);
+    constructor(props){
+        super(props);
 
-    this.state = {
-      jsonData: {},  
-      itens: {}
-    };
+        this.state = {
+            jsonData: {},  
+            itens: {}
+        };
 
-    this.recursiveData = this.recursiveData.bind(this);
-    this.onToggle = this.onToggle.bind(this);
-  }
-
-  onToggle(node, toggled){
-    if(this.state.cursor){this.state.cursor.active = false;}
-    node.active = true;
-    if(node.children){ node.toggled = toggled; }
-    this.setState({ cursor: node });
-  }
-
-  recursiveData(item, data){
-    treeData = treeData + "{";
-    treeData = treeData + "\"id\":" + "\"" + item.id + "\", ";
-    treeData = treeData + "\"name\":" + "\"" + item.name + "\"";
-    if(item.hasOwnProperty("children")){
-      treeData = treeData + ", \"children\":" + "[";
-      for(let i = 0; i < item.children.length; i++) {
-        this.recursiveData(item.children[i]);
-        if(i < (item.children.length - 1)) {
-          treeData = treeData + ",";
-        }
-      }
-      treeData = treeData + "]";
+        this.recursiveData = this.recursiveData.bind(this);
+        this.onToggle = this.onToggle.bind(this);
     }
 
-    if(item.hasOwnProperty("devices")){
-      treeData = treeData + ", \"children\":" + "[";
-
-      for(let i = 0; i < item.devices.length; i++) {
-        //console.log("-", item.children[i].name );
-        this.recursiveData(item.devices[i]);
-        if(i < (item.devices.length - 1)) {
-          treeData = treeData + ",";
-        }
-      }
-
-      treeData = treeData + "]";
+    onToggle(node, toggled){
+        if(this.state.cursor){this.state.cursor.active = false;}
+        node.active = true;
+        if(node.children){ node.toggled = toggled; }
+        this.setState({ cursor: node });
     }
-    treeData = treeData + "}";
 
-    // console.log("", treeData );
-  }
+    recursiveData(item, data){
+        treeData = treeData + "{";
+        treeData = treeData + "\"id\":" + "\"" + item.id + "\", ";
+        treeData = treeData + "\"name\":" + "\"" + item.name + "\"";
+            if(item.hasOwnProperty("children") || item.hasOwnProperty("devices") ){
+                treeData = treeData + ", \"children\":" + "[";
 
-  componentDidMount(){
-    fetch('https://zh7k3p5og1.execute-api.us-east-1.amazonaws.com/testing/environments')
-    .then(res => res.json())
-    .then(json => {
+                if(item.hasOwnProperty("children")){
+                    for(let i = 0; i < item.children.length; i++) {
+                        this.recursiveData(item.children[i]);
+                        if(i < (item.children.length - 1)) {
+                            treeData = treeData + ",";
+                        }
+                    }
+                }
 
-      json.data.environments.map(item => (
-        this.recursiveData(item, treeData)
-      ));
+                if(item.hasOwnProperty("devices")){
+                    if(item.hasOwnProperty("children")){
+                        treeData = treeData + ",";
+                    }
 
-      var cama = JSON.parse(treeData);
-      this.setState({
-        jsonData: cama
-      });
-    });
-  }
+                    for(let i = 0; i < item.devices.length; i++) {
+                    //console.log("-", item.children[i].name );
+                        this.recursiveData(item.devices[i]);
+                        if(i < (item.devices.length - 1)) {
+                            treeData = treeData + ",";
+                        }
+                    }
+                }
 
-  render(){
-    return (
+                treeData = treeData + "]";
+            }
 
-      <Treebeard
-        data={this.state.jsonData}
-        onToggle={this.onToggle}
-      />
-    );
-  }
+        treeData = treeData + "}";
+
+        console.log("", treeData );
+
+    }
+
+    componentDidMount(){
+        fetch('https://zh7k3p5og1.execute-api.us-east-1.amazonaws.com/testing/environments')
+        .then(res => res.json())
+        .then(json => { 
+
+            json.data.environments.map(item => (
+                this.recursiveData(item, treeData)
+            ));
+
+            var data = JSON.parse(treeData);
+            this.setState({
+                jsonData: data
+            })
+    
+
+        });
+    }
+
+    render(){
+        return (
+
+
+            <Treebeard
+                data={this.state.jsonData}
+                onToggle={this.onToggle}
+            />
+        );
+    }
+
 
 }
 
