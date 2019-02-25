9cbc4a2b6e20c273921e7673e0257c1e0572bd12
mymrtt <mymrtt@gmail.com>
2019-02-25 13:30:39 -0300

    Updates

--- precache-manifest.a40fc2513d141f5d22882834b8fccee5.js
+++ precache-manifest.a40fc2513d141f5d22882834b8fccee5.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/lowitproject/static/js/runtime~main.5a0a00ec.js"
   },
   {
-    "revision": "46cc4d004c74f73a8a85",
-    "url": "/lowitproject/static/js/main.46cc4d00.chunk.js"
+    "revision": "2f4aa2d98767e009dd25",
+    "url": "/lowitproject/static/js/main.2f4aa2d9.chunk.js"
   },
   {
     "revision": "87d7e2ac280c39e043d5",
     "url": "/lowitproject/static/js/2.87d7e2ac.chunk.js"
   },
   {
-    "revision": "46cc4d004c74f73a8a85",
-    "url": "/lowitproject/static/css/main.6532775b.chunk.css"
+    "revision": "2f4aa2d98767e009dd25",
+    "url": "/lowitproject/static/css/main.2f3d0cf8.chunk.css"
   },
   {
-    "revision": "2c5c38110a09f60f7a3ed973ab7127dd",
+    "revision": "cfdf2e3c6312d7553d8318ae531d3a95",
     "url": "/lowitproject/index.html"
   }
 ];
