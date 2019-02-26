ae6f969e5a691ed296ba8698a42b5d7ae35a4b56
mymrtt <mymrtt@gmail.com>
2019-02-26 16:14:59 -0300

    Updates

--- precache-manifest.8206041daabbabb90bdf353e468f0e39.js
+++ precache-manifest.8206041daabbabb90bdf353e468f0e39.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/lowitproject/static/js/runtime~main.5a0a00ec.js"
   },
   {
-    "revision": "09586a65abd6b68264c7",
-    "url": "/lowitproject/static/js/main.09586a65.chunk.js"
+    "revision": "7029b6744458b5d4e39e",
+    "url": "/lowitproject/static/js/main.7029b674.chunk.js"
   },
   {
     "revision": "87d7e2ac280c39e043d5",
     "url": "/lowitproject/static/js/2.87d7e2ac.chunk.js"
   },
   {
-    "revision": "09586a65abd6b68264c7",
-    "url": "/lowitproject/static/css/main.c43662ca.chunk.css"
+    "revision": "7029b6744458b5d4e39e",
+    "url": "/lowitproject/static/css/main.51d56afe.chunk.css"
   },
   {
-    "revision": "a55825e4aa3a323c2b8a772d241664c4",
+    "revision": "3c7c714041d6473d625ab1f65d6d20f8",
     "url": "/lowitproject/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/lowitproject/precache-manifest.423861147252be7b5cdfccf9aa4a8f92.js"
+  "/lowitproject/precache-manifest.8206041daabbabb90bdf353e468f0e39.js"
 );
 
 workbox.clientsClaim();
